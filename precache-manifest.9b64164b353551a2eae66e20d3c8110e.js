ec06c44b2f331de9379c6a55b6fcdfa0e3f40a05
Salekin Imran <abir045@gmail.com>
2021-01-29 00:11:38 +0600

    Updates

---
+++ precache-manifest.9b64164b353551a2eae66e20d3c8110e.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9475f0a31b0a21c9b0611bcbe774077d",
+    "url": "/xtremeFitness/index.html"
+  },
+  {
+    "revision": "366daf69fa97bb497979",
+    "url": "/xtremeFitness/static/css/2.fed0eb58.chunk.css"
+  },
+  {
+    "revision": "366daf69fa97bb497979",
+    "url": "/xtremeFitness/static/js/2.f127c2c3.chunk.js"
+  },
+  {
+    "revision": "f8325e8be227ca30c5db",
+    "url": "/xtremeFitness/static/js/main.4d2e9718.chunk.js"
+  },
+  {
+    "revision": "a7615e382a34dc3cfd39",
+    "url": "/xtremeFitness/static/js/runtime-main.14234120.js"
+  },
+  {
+    "revision": "e30943632aabcd98b2054aff30d95915",
+    "url": "/xtremeFitness/static/media/bg1.e3094363.jpg"
+  },
+  {
+    "revision": "0865053fe652971dcfd480feb1354c5c",
+    "url": "/xtremeFitness/static/media/bg2.0865053f.jpg"
+  },
+  {
+    "revision": "b274ed1a724e97bf4ebf0f221b257d3b",
+    "url": "/xtremeFitness/static/media/bg4.b274ed1a.jpg"
+  },
+  {
+    "revision": "503bed54e7814ef894c5daf81ce7f4de",
+    "url": "/xtremeFitness/static/media/bg5.503bed54.jpg"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/xtremeFitness/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/xtremeFitness/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "a9c4bb7348f42626454c988dbde1d0a0",
+    "url": "/xtremeFitness/static/media/fa-brands-400.a9c4bb73.svg"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/xtremeFitness/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/xtremeFitness/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/xtremeFitness/static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "7b9568e6389b1f8ae0902cd39665fc1e",
+    "url": "/xtremeFitness/static/media/fa-regular-400.7b9568e6.svg"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "/xtremeFitness/static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/xtremeFitness/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/xtremeFitness/static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/xtremeFitness/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/xtremeFitness/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/xtremeFitness/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/xtremeFitness/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "c2801fb415f03c7b170934769d7b5397",
+    "url": "/xtremeFitness/static/media/fa-solid-900.c2801fb4.svg"
+  },
+  {
+    "revision": "f503b74b3e5136831b4a31f5d04ddfd9",
+    "url": "/xtremeFitness/static/media/logo.f503b74b.jpg"
+  },
+  {
+    "revision": "c3136d35c86995f3d61f870b155842fd",
+    "url": "/xtremeFitness/static/media/tr1.c3136d35.jpg"
+  },
+  {
+    "revision": "12bc17188a1f04d8ef48382bc4f75029",
+    "url": "/xtremeFitness/static/media/tr2.12bc1718.jpg"
+  },
+  {
+    "revision": "9f10049e4d17477bf74c77569fa5123e",
+    "url": "/xtremeFitness/static/media/tr3.9f10049e.jpg"
+  }
+]);
